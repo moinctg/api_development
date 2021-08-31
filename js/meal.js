75f789843178d0dabe2397033e8c2802336298cd
Moinuddin Kamal <IT@pronayonbd.com>
2021-08-31 17:51:56 +0600

    update meal db pic

--- js/meal.js
+++ js/meal.js
@@ -2,32 +2,34 @@ const loadSerachItem = () => {
 
     const serchField = document.getElementById('searchInput');
     const searchText = serchField.value;
-    console.log(searchText);
+    // console.log(searchText);
     serchField.value = '';
 
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     // console.log(url)
     fetch(url)
     .then(res => res.json())
-    .then(data => displaySerchItem(data))
+    .then(data => displaySearchItem(data.meals))
 }
 
-const displaySerchItem = meals => {
+const displaySearchItem = meals => {
+  console.log(meals);
  const containerDiv = document.getElementById('card');
  meals.forEach( meal => {
+    // console.log(meal);
    const div = document.createElement('col');
    div.classList.add('col');
    div.innerHTML =  `
           <div class="card h-100">
-            <img src="" class="card-img-top" alt="...">
+            <img  src=${meal.strMealThumb} class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text"> ${meal.strInstructions.slice(0-250)}</p>
             </div>
           </div>
            `;
         containerDiv.appendChild(div);
 
- })
-  console.log(meal);
+ });
+//   console.log(meal);
 }
